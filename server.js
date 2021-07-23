e5dc88490de3f8a48f69b2fd8c65d2d8f5bff5cc
ankitbcrec <ankit.bcrec@gmail.com>
2021-07-23 16:35:05 +0530

    Validation Message

--- server.js
+++ server.js
@@ -16,13 +16,27 @@ app.listen(3000, () =>{
 });
 
 //redirecting to the web browser http://localhost:3000/
-app.get('/', (req,res) =>{
+/*
+  app.get('/', (req,res) =>{
   res.send("hello world!");
 });
+*/
 
+//  code block to expose the api
 app.post('/', (req,res) =>{
   var email = req.body.email
   var amount = req.body.amount
+
+//  code block to validate if the lottery amount is less than x values
+
+  if(amount <= 1){
+    return_info = {};
+    return_info.error = true;
+    return_info.message = "The amount must be greate than 1";
+
+    return res.send(return_info);
+  }
+
 
   res.send({"amount" : amount, "email" : email});
 });
